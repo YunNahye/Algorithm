5e13888805b5e1ccbab06b484ae33030afbae893
YunNahye <zuu0319@ajou.ac.kr>
2022-08-16 15:33:41 +0900

    (LeetCode) 200. Number of Islands [Medium]

---
+++ LeetCode/DFS&BFS/200.js
@@ -0,0 +1,25 @@
+var numIslands = function(grid) {
+  let result = 0;
+  
+  const dfs = (x, y) => {
+    if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0) return;
+    
+    grid[x][y] = 0;
+    
+    dfs(x, y+1);
+    dfs(x+1, y);
+    dfs(x-1, y);
+    dfs(x, y-1);
+  };
+  
+  for(let i=0; i<grid.length; i++) {
+    for(let j=0; j<grid[0].length; j++) {
+      if(grid[i][j] == 1) {
+        dfs(i, j);
+        result++;
+      }
+    }
+  }
+  
+  return result;
+};
